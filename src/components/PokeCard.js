58a638ae8230096f537f0d257cfcc0920d4feaaf
Latch Jack <latch.jack@gmail.com>
2020-07-02 16:40:00 +0100

    added code to index and fixing card

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PokeCard = ({ name }) => (
-  <div>
+  <div className="card-border">
     <Link to={`/pokemon/${name}`} >
       <h1>{name}</h1>
     </Link>
--- src/components/PokeShow.js
+++ src/components/PokeShow.js
@@ -10,6 +10,7 @@ class PokeShow extends React.Component {
     const name = this.props.match.params.id
     try {
       const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      console.log('component did mount')
       console.log(res.data)
       this.setState({ pokemon: res.data.results })
     } catch(err) {
@@ -17,13 +18,17 @@ class PokeShow extends React.Component {
     }
   }
 
+
   render() {
-    const { name, id, species, height, weight } = this.state.pokemon
+    console.log('rendering page')
+    const { data, name, id, species, height, weight } = this.state
+    // if (!data) return <div className="loading"></div>
     return (
       <div>
         <p>poke</p>
         <h1>{name}</h1>
         <p>{id}</p>
+        {/* <img src={sprites.front_default} /> */}
         <p>{species}</p>
         <p>{height}</p>
         <p>{weight}</p>
